777de04714042283a4169954d05cda4239f16d4b
surajdevesan <surajd@qburst.com>
2017-09-30 04:46:43 +0530

    Added material design

--- client/components/DiscussionComponent.js
+++ client/components/DiscussionComponent.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import ProjectDetails from './ProjectDetailsComponenet';
+import ProjectDetails from './ProjectDetailsComponent';
 import ChatList from './ChatListComponent';
+import RaisedButton from 'material-ui/RaisedButton';
 import PostChat from '../containers/PostChatContainer';
 export default class Discussion extends React.Component {
+  buttonClick() {
+    console.log('@on click');
+  }
   render() {
+    const style = {
+      margin: 12,
+    };
     return (
       <div>
-        <ProjectDetails />
-        <ChatList />
-        <PostChat />
+        {/* <ProjectDetails /> */}
+        {/* <ChatList /> */}
+        {/* <PostChat /> */}
+        <RaisedButton
+          onClick = {this.buttonClick}
+          label="Default" style={style} />
       </div>
     );
 
--- client/routes.js
+++ client/routes.js
@@ -4,12 +4,21 @@ import DiscussionComponent from './components/DiscussionComponent.js';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import reducer from './reducers';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
+injectTapEventPlugin();
 const store = createStore(reducer);
 
-ReactDOM.render(
+const App = () => (
   <Provider store={store}>
-    <DiscussionComponent/>
-  </Provider>,
+    <MuiThemeProvider>
+      <DiscussionComponent/>
+    </MuiThemeProvider>
+  </Provider>
+);
+
+ReactDOM.render(
+  <App />,
   document.getElementById('app')
 );
